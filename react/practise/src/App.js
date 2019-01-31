4b3e0a3b22616627ff8a7fcdc1b9ee8a5cfe65d6
sensemin7 <sensemin7@gmail.com>
2019-02-01 00:06:36 +0900

    practise

---
+++ react/practise/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import Contect from './Contect';
+class App extends Component {
+  render() {
+    return (
+     <Contect />
+    )
+  }
+}
+
+export default App;
---
+++ react/practise/src/Contect.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Contect extends Component {
+    
+    state() {}
+    render() {
+        return(
+            <div>
+                <h1>연습중</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Contect;
